7f260b76bca7e7cb0de2732d77f8d41de751a424
Katie Hairston <kth5sf@virginia.edu>
2020-02-11 18:29:12 -0500

    changes to service implementations

--- src/main/java/dev/KAMA/services/TeacherService.java
+++ src/main/java/dev/KAMA/services/TeacherService.java
@@ -1,9 +1,21 @@
 package dev.KAMA.services;
 
 
+import java.util.Set;
+
+import dev.KAMA.entities.Report;
+import dev.KAMA.entities.Showcase;
 import dev.KAMA.entities.Teacher;
 
 public interface TeacherService {
 
-	public Teacher getTeacherByUsername(String username);
+	public Teacher loginTeacher(String username, String password);
+	
+	public Teacher submitReport(Teacher teacher, Report report);
+	
+	public Set<Report> viewAllReports();
+	
+	public Teacher submitShowcase(Teacher teacher, Showcase showcase);
+	
+	public Set<Showcase> viewAllShowcases();
 }
