018688bdd3a93470370bed2b583a8f9ff1d9d40e
anthonylucasmcarthur <60712819+anthonylucasmcarthur@users.noreply.github.com>
2020-02-11 12:37:20 -0500

    spring boot projec

---
+++ src/main/java/com/example/demo/KamaProject2BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamaProject2BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KamaProject2BackEndApplication.class, args);
+	}
+
+}
