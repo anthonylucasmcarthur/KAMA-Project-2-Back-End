9eb1481271016fe36debb5741ae437e6ff90a013
Aaleyah4d <60712697+Aaleyah4d@users.noreply.github.com>
2020-02-13 12:27:52 -0500

    uploaded test

--- src/test/java/dev/KAMA/app/KamaProject2BackEndApplicationTests.java
+++ src/test/java/dev/KAMA/app/KamaProject2BackEndApplicationTests.java
@@ -35,21 +35,62 @@ class KamaProject2BackEndApplicationTests {
 	@Autowired
 	TeacherService ts;
 	
-	@Test
-	void contextLoads() {
-		System.out.println("Hello");
-	}
-	
-	@Test
-	void findAllChild() {
-		Child c = new Child();
-		Teacher t = new Teacher();
-		t = tr.findById(2).get();
-		c = cr.findById(8).get();
-		//System.out.println(c.getReports());
-		//System.out.println(rr.findByChild(c));
-		System.out.println(rr.findByTeacher(t));
-		//System.out.println(sr.findAll());
-	}
-
+	 @Test
+ void contextLoads() {
+  System.out.println("Hello");
+ }
+ // testing services
+ // teacher
+ @Test
+ void findAllChild() {     
+  System.out.println(pr.findByUsername("ablack"));
+  System.out.println(ts.findAllChildren());
+ }
+ // not working ... need help
+ @Test
+ void viewAllReports(){
+ 
+  
+  System.out.println(ts.viewAllReports());
+ }
+ // this works
+ @Test
+ void viewAllShowcases(){
+  System.out.println(ts.viewAllShowcases());
+ }
+ 
+ 
+ // works
+ @Test
+ void submitReport() {
+  Report r = new Report();
+  Teacher t = tr.findByUsername("cgreen");
+  
+  r.setStatus("Bad");
+  r.setComment("pushed kid off swing");
+ 
+ 
+  ts.submitReport(t, r);
+ }
+ @Test
+ void submitShowcase() {
+  Showcase sc = new Showcase();
+  Teacher t = tr.findByUsername("cgreen");
+  Child c = new Child();
+  c.setcId(1);
+  sc.setLink("https://www.google.com/");
+  sc.setComment("he did good");
+  sc.setChild(c);
+  ts.submitShowcase(t, sc);
+ }
+ 
+ // test for the parent
+ 
+ @Test
+ void loginParent() {
+  System.out.println(pr.findByUsername("wwest"));
+ }
+ 
+ 
+ 
 }
